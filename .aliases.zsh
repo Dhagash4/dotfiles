267eb47191c97d92e44f15677fec9b4a6a727c28
Dhagash Desai <desaidhagash@gmail.com>
2023-02-19 23:20:31 +0100

    zsh changes

---
+++ .aliases.zsh
@@ -0,0 +1,30 @@
+#alias -g latest='*(om[1])'
+alias py='ipython3'
+alias coit='code . && exit'
+alias cor='code -r'
+
+#alias deko='cd ~/Desktop'
+#alias ex='cd ~/dev/examples'
+#alias te='cd ~/Teaching'
+#alias de='cd ~/dev'
+#alias re='cd ~/Research/'
+
+alias xopen='xdg-open'
+alias xcopy='xclip -selection clipboard'
+alias ll='ls -hltrBF --group-directories-first --color=auto'
+alias df='df -h -xsquashfs -xtmpfs -xdevtmpfs'
+#alias tkb='take build'
+#alias vim='nvim'
+alias tmux='tmux -2'
+alias ts='tmux new-session -s $(basename $(pwd))'
+#alias ydam='yadm'
+#alias mkae='make'
+#alias cat='bat'
+#alias meld='light_meld'
+alias gs="git status"
+
+#alias cm="cmake -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+#alias cb="cmake --build build -j$(nproc --all)"
+
+#my aliases
+alias cc="clear"
