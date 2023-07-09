1b4444daae099072262e2ec95b35a602ed8369ea
Dhagash Desai <desaidhagash@gmail.com>
2023-07-09 22:49:20 +0200

    basic utility added

---
+++ .aliases.zsh
@@ -0,0 +1,14 @@
+alias py='ipython3'
+#alias coit='code . && exit'
+alias cor='code -r'
+
+alias xopen='xdg-open'
+alias xcopy='xclip -selection clipboard'
+alias ll='ls -hltrBF --group-directories-first --color=auto'
+alias df='df -h -xsquashfs -xtmpfs -xdevtmpfs'
+alias vim='nvim'
+alias ydam='yadm'
+alias gs="git status"
+
+alias c="clear"
+alias r="source ~/.zshrc"
