8edbe5131d4d8eea3137eb3df3c546c23440aeb7
Dhagash Desai <desai.dhagash@gmail.com>
2024-09-12 16:21:42 +0200

    minor addition

---
+++ .ros.sh
@@ -0,0 +1,13 @@
+#Credits: Igancio Vizzo (https://github.com/nachovizzo/dotfiles/blob/main/.ros.sh)
+
+#!/usr/bin/env zsh
+# Fix zsh autocomplete in zsh
+# https://github.com/ros2/ros2cli/issues/534#issuecomment-957516107
+
+# Fix zsh autocomplete in zsh
+
+if [ -f /opt/ros/${ROS_DISTRO}/setup.zsh ]; then
+  source /opt/ros/${ROS_DISTRO}/setup.zsh
+  eval "$(register-python-argcomplete3 ros2)"
+  eval "$(register-python-argcomplete3 colcon)"
+fi
